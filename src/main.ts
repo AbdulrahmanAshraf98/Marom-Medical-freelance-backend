5a6341f228b38ea8cfa69d762cf5023a51fc138d
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2024-07-20 17:52:00 +0300

    integration with swagger apis

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   const configService: ConfigService = app.get(ConfigService);
   const port: number = configService.get<number>('APP_PORT', 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Marom Medical')
+    .setDescription('The Marom Medical API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.useGlobalPipes(new ValidationPipe());
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
