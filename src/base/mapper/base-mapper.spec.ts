f078d09bcb9d9150f04daac1f18ebf28ca8fc749
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2024-07-22 20:24:28 +0300

    base mapper

--- src/base/mapper/base-mapper.spec.ts
+++ src/base/mapper/base-mapper.spec.ts
@@ -6,21 +6,24 @@ describe('BaseMapper', () => {
   it('should be defined', () => {
     expect(new BaseMapper()).toBeDefined();
   });
-  it('should map map a single entity to a DTO',()=>{
-    const entity:DummyEntity = new DummyEntity(1, 'Test Name', true);
-    const  dto:TestDto= BaseMapper.toDto<TestDto,DummyEntity>(TestDto,entity);
+  it('should map map a single entity to a DTO', () => {
+    const entity: DummyEntity = new DummyEntity(1, 'Test Name', true);
+    const dto: TestDto = BaseMapper.toDto<TestDto, DummyEntity>(
+      TestDto,
+      entity,
+    );
     expect(dto).toBeInstanceOf(TestDto);
     expect(dto.id).toEqual(entity.id);
     expect(dto.name).toEqual(entity.name);
     expect(dto.is_active).toBeUndefined();
-  })
+  });
   it('should map an array of entities to an array of DTOs', () => {
-    const entities:DummyEntity[] = [
+    const entities: DummyEntity[] = [
       new DummyEntity(1, 'Test Name 1', true),
       new DummyEntity(2, 'Test Name 2', false),
     ];
 
-    const dtos:TestDto[] = BaseMapper.toDtos(TestDto, entities);
+    const dtos: TestDto[] = BaseMapper.toDtos(TestDto, entities);
     expect(dtos).toHaveLength(2);
     expect(dtos[0]).toBeInstanceOf(TestDto);
     expect(dtos[1]).toBeInstanceOf(TestDto);
@@ -30,9 +33,9 @@ describe('BaseMapper', () => {
     expect(dtos[1].is_active).toBeUndefined();
   });
   it('should map a plain object to a DTO', () => {
-    const plainObject:object = { id: 1, name: 'Test Name', is_active: true };
+    const plainObject: object = { id: 1, name: 'Test Name', is_active: true };
 
-    const dto:TestDto = BaseMapper.toDtoFromPlain(TestDto, plainObject);
+    const dto: TestDto = BaseMapper.toDtoFromPlain(TestDto, plainObject);
 
     expect(dto).toBeInstanceOf(TestDto);
     expect(dto.id).toEqual(1);
